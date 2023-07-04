b3eb14016c87865ecd5cb1ede903749660e94bad
AH400796 <a.hokhman@gmail.com>
2023-07-04 12:46:00 +0300

    Update Error.tsx

--- app/Error.tsx
+++ app/Error.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 import { Wrapper } from "@styles/styled/Error.styled";
 
 const ErrorPage: React.FC = () => {
-  return <Wrapper>Error</Wrapper>;
+  return (
+    <Wrapper>
+      <h1>Error</h1>
+      <p>Something goes wrong... </p>
+      <p>Redirected to mainpage... </p>
+    </Wrapper>
+  );
 };
 
 export default ErrorPage;
